7c8f2d8195fd59c086480a5b2af791f2d115919d
kopdejana <kopdejanacs@gmail.com>
2025-02-26 14:19:39 +0100

    [add] Edit Note link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,11 +9,32 @@ function Home() {
             title: "First",
             content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur laborum nemo autem, a aliquam id reprehenderit officiis. Reiciendis molestiae nemo consequatur sapiente sed repellat minus officia dolorum fugit? Modi, vel?",
             color: "pink"
-        }
+        },
+        {
+            id: 2,
+            title: "Second",
+            content: "Lorem ipsum dolor sit amet",
+            color: "gray"
+        },
+        {
+            id: 3,
+            title: "third jajslkajskla hdjsahjds",
+            content: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
+            color: "blue"
+        },
+        {
+            id: 4,
+            title: "Fourth",
+            content: "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet",
+            color: "blue"
+        },
     ];
 
     const notesComponents = notes.map(
-        note => <Note title={note.title} content={note.content} color={note.color}/>
+        note => 
+        <Link to={"/edit-note/" + note.id}>
+            <Note title={note.title} content={note.content} color={note.color}/>
+        </Link>
     )
 
     return (
